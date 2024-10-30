65eabb56521fe56392aa47cbd395520e2bdfece2
Kevin <kevinantoniowiyonolauw@mail.ugm.ac.id>
2024-10-30 21:59:00 +0700

    Update

--- olclass-app/src/components/FoodList.js
+++ olclass-app/src/components/FoodList.js
@@ -205,7 +205,6 @@ const settings = isFavorites ? {
                 )}
 
             </Slider>
-
             {/* Modal */}
             {selectedFood && (
                 <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 transition-opacity duration-300">
